43034422502dfaa24574e37fe1a12be1a92ab036
Andrei L <1881266+unrevised6419@users.noreply.github.com>
2025-12-26 16:59:18 +0200

    fix: move inter font to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
-
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -26,11 +16,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" className={inter.variable}>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
+			<body className={`${inter.variable} antialiased`}>{children}</body>
 		</html>
 	);
 }
