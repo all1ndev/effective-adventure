eedc5d9dc9179632b8bb2d39824798fc3eb78d86
Veronica Luca <41445396+veroniespera@users.noreply.github.com>
2026-02-26 21:46:51 +0200

    feat: add vital signs tracking feature

---
+++ src/app/(authenticated)/vital-signs/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { RoleGuard } from "@/components/role-guard";
+import { VitalSigns } from "@/features/vital-signs";
+
+export default function VitalSignsPage() {
+	return (
+		<RoleGuard allowedRoles={["pacient"]}>
+			<VitalSigns />
+		</RoleGuard>
+	);
+}
